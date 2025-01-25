a5c57fbbb0316008f16a5116179e495ba7153262
Joona Pellinen <joonaaleks@gmail.com>
2025-01-25 12:37:57 +0200

    Player damage and enemy hp

--- scenes/enemies/lisko_enemy.gd
+++ scenes/enemies/lisko_enemy.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
-
 var movement_speed: float = 200.0
 var movement_target_position: Vector2 = Vector2(60.0,180.0)
 
+# Health
+@export var health: int
+@export var damage: int
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
 func _ready():
@@ -28,7 +30,6 @@ func set_movement_target(movement_target: Vector2):
 func _physics_process(delta):
 	if navigation_agent.is_navigation_finished():
 		return
-
 	var current_agent_position: Vector2 = global_position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 
