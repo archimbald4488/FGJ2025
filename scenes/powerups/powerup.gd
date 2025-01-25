f045f2f87bd701b6fa6a7a5ccbf909e04a9b4146
Joona Pellinen <joonaaleks@gmail.com>
2025-01-25 21:01:20 +0200

    Powerup rng and despawn

--- scenes/powerups/powerup.gd
+++ scenes/powerups/powerup.gd
@@ -1,8 +1,18 @@
 extends Area2D
 
-func _ready() -> void:
-	pass
-	
 #Check when player is in powerup hitbox
 func _on_body_entered(body: Node2D) -> void:
+	#Randomly assign powerup type on pickup
+	var rng = randi_range(1, 4)
+	if rng == 1:
+		print("You got sex")
+	if rng == 2:
+		print("+1000 aura")
+	if rng == 3:
+		print("+1 megis")
+	if rng == 4:
+		print("k")
+	queue_free()
+	
+func _on_pow_despawn_timeout() -> void:
 	queue_free()
